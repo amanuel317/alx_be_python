cfa132f420d76df2d03ef4391de18c9ad27e517d
amanuel317 <125757757+amanuel317@users.noreply.github.com>
2024-06-20 15:38:25 +0000

    Subtraction and Multiplication Added

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,3 +5,5 @@ Subtraction of 10 and 5 is 5
 Multiplication of 10 and 5 is 50"""
 
 print(f"Addition of {number1} and {number2} + is {number1+number2}")
+print(f"Subtraction of {number1} and {number2} is {number1-number2}")
+print(f"Multiplication of {number1} and {number2} is {number1*number2}")
