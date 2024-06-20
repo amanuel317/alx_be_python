3cb3e7ce5877878dd7660b9d58c99ac89601d98e
amanuel317 <125757757+amanuel317@users.noreply.github.com>
2024-06-20 16:36:59 +0300

    print sum of two numbers

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,2 +1,8 @@
-number1=5
-number2=10
+number1=10
+number2=2
+
+"""Addition of 10 and 5 is 15
+Subtraction of 10 and 5 is 5
+Multiplication of 10 and 5 is 50"""
+
+print("Addition of " + number1 + " and " + number2 + " is " + number1+number2)
