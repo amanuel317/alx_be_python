3088d52832237722bd83341b6f7dcecb302c7008
amanuel317 <125757757+amanuel317@users.noreply.github.com>
2024-06-20 18:39:25 +0000

    edited simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,5 +3,5 @@ rate = 0.05
 time = 3
 
 # Calculate simple interest
-simple_interest = (principal * rate * time)
-print(f"The simple interest is: {simple_interest}")
+interest = (principal * rate * time)
+print(f"The simple interest is: {interest}")
