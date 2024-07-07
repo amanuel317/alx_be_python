c075b53051a1f1a822be1c00441fed1f7036d2c0
amanuel317 <amanuelabraham2215@gmail.com>
2024-07-07 17:47:04 +0300

    added elif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,5 +11,5 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Can't divide by 0"
-            else:
+            elif(num2>0):
                 return num1/num2
